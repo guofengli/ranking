87b488eb45772cfe5ec6a853d820d411f41dd520
guofengli <liguofeng@elex-tech.com>
2015-04-07 18:58:44 +0800

    添加连接表是天数

--- src/main/java/com/elex/hive/AresMerge.java
+++ src/main/java/com/elex/hive/AresMerge.java
@@ -1,9 +1,12 @@
 package com.elex.hive;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class AresMerge {
 	public static String urlStr = "jdbc:hive2://namenode:10000/default";
@@ -39,10 +42,14 @@ public class AresMerge {
 		return mark;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		 Properties prop = new Properties();
+		 prop.load(AresMerge.class.getClassLoader().getResourceAsStream("conf.properties"));
+		 String startDay = prop.getProperty("startday");
+		 String endDay = prop.getProperty("endday");
 		String sql = "insert overwrite table ares_merge1 select t.campid, t.nation, t.size, t.language, t.site, t.slot, t.browser, t.time, sum(c.confirmed_sum), sum(c.confirmed_dollar), sum(c.other_sum), sum(c.other_dollar), max(c.max_delay), count(t.reqid),count(c.clickid) "
 				+ "from (select uid as uid, reqid as reqid,nation as nation,size as size,language as language,site as site,slot as slot,browser as browser,timeframe(nation,time) as time,camp as campid,ad as adid "
-				+ "from ares_impression lateral view splitcampidudtf(concat(camp_id,':',adid)) udtf as camp, ad) "
+				+ "from ares_impression lateral view splitcampidudtf(concat(camp_id,':',adid)) udtf as camp, ad where (day >=" + startDay + "and day <= " + endDay + ")) "
 				+ "t left join (select uid as uid, reqid as reqid, count(clickid) as clickid, campid as campid, sum(confirmed_sum) as confirmed_sum, sum(confirmed_dollar) as confirmed_dollar, sum(other_sum) as other_sum, sum(other_dollar) as other_dollar,max(max_delay) as max_delay "
 				+ "from ares_click_trans where (campid is not null and reqid is not null and clickid is not null) group by reqid, campid, uid) "
 				+ "c on (t.uid = c.uid and t.reqid = c.reqid and t.campid = c.campid) group by t.nation, t.size, t.language, t.site, t.slot, t.browser, t.time, t.campid";
